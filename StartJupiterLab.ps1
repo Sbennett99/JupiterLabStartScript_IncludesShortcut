851fe3119f02dc53bd7f35aefcf010643682f0d5
Seth Bennett <54418905+Sbennett99@users.noreply.github.com>
2022-01-20 12:44:53 -0600

    Add files via upload

---
+++ StartJupiterLab.ps1
@@ -0,0 +1,4 @@
+cd C:\Users\S530995\Documents\WebMining # Your environment location Here
+Start-Process "http://localhost:8888/lab/"
+.\wm-py310env\Scripts\activate # change name to that of your environment
+jupyter notebook
